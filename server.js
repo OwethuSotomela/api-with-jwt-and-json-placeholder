7db027ac86207fef88685dff97a8eb0c8dcc572d
OwethuSotomela <owethusotomela@gmail.com>
2022-04-18 19:23:45 +0200

    API with JWT

--- server.js
+++ server.js
@@ -1,42 +1,53 @@
-require('dotenv').config()
-
 const express = require("express");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
-app.use(express.json());
+app.get('/api', (req, res) => {
+    res.json({
+        message: "Welcome to the API"
+    })
+})
 
-const posts = [
-    {
+app.get('/api/post', verifyToken, (req, res) => {
+    res.json({
+        post: "Post created"
+    })
+})
+
+app.post('/api/login', (req, res) => {
+    const user = {
+        id: 1,
         username: 'Owe',
-        title: 'Post 1'
-    },
-    {
-        username: 'Wethu',
-        title: 'Post 2'
+        email: 'owe@gmail.com'
     }
-    
-]
 
-app.get('/posts', (req, res)=>{
-    res.json(posts);
-});
+    jwt.sign({ user }, 'secretkey', (err, token) => {
+        res.json({
+            token
+        })
+    })
 
-app.post('/login', authenticateToken, (req, res)=>{
-    // Authenticate user
+})
 
-    const username =  req.body.username;
-    const user = { name: username }
+// Format of token
+// Authoization: <access token>
 
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({ accessToken: accessToken})
-});
-
-function authenticateToken(req, res, next){
-
+// verify token
+function verifyToken(req, res, next) {
+    // get auth header value 
+    const bearerHeader = req.headers['authorization'];
+    // check if not undefined
+    if (typeof bearerHeader !== 'undefined') {
+        // splint at the space
+        const bearer = bearerHeader.split(' ');
+        // get token from array
+    } else {
+        // forbidden
+        res.sendStatus(403)
+    }
 }
 
-console.log("Hello, world!");
+console.log("Hello, Oz!");
 
 app.listen(3000);
